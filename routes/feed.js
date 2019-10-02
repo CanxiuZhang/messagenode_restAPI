3a7792207c71c72ba5ea2f060afcc70d742c27c1
Canxiu Zhang <19885367+CanxiuZhang@users.noreply.github.com>
2019-10-02 11:34:43 -0400

    add server side validation

--- routes/feed.js
+++ routes/feed.js
@@ -4,10 +4,15 @@ const feedController = require('../controllers/feed');
 
 const router = express.Router();
 
+const {body} = require('express-validator');
+
 // GET /feed/posts
 router.get('/posts', feedController.getPosts);
 
 // POST /feed/post
-router.post('/post', feedController.createPost);
+router.post('/post', [
+  body('title').trim().isLength({min: 7}),
+  body('content').trim().isLength({min: 5})
+], feedController.createPost);
 
 module.exports = router;
